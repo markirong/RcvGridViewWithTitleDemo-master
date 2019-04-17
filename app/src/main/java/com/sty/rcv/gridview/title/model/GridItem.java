29285e235b607c6c0e05da423f6fa297e57fe462
caijianrong <2391786774@qq.com>
2019-04-17 15:27:54 +0800

    Initial commit

---
+++ app/src/main/java/com/sty/rcv/gridview/title/model/GridItem.java
@@ -0,0 +1,25 @@
+package com.sty.rcv.gridview.title.model;
+
+
+
+public class GridItem {
+    private String text;
+    private int drawableId;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getDrawableId() {
+        return drawableId;
+    }
+
+    public void setDrawableId(int drawableId) {
+        this.drawableId = drawableId;
+    }
+}
+
